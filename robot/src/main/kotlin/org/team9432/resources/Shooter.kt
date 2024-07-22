be3215ef5081e3055a407968fab32de0efd794a6
Nolan Locke <nolan.s.locke@gmail.com>
2024-07-22 14:41:44 -0700

    shooter simplification and tuning

--- robot/src/main/kotlin/org/team9432/resources/Shooter.kt
+++ robot/src/main/kotlin/org/team9432/resources/Shooter.kt
@@ -1,48 +1,28 @@
 package org.team9432.resources
 
-import edu.wpi.first.math.controller.PIDController
 import org.team9432.lib.resource.Resource
-import org.team9432.lib.robot.CoroutineRobot
 import org.team9432.lib.wrappers.Spark
 import org.team9432.lib.wrappers.neo.LoggedNeo
 
 object Shooter: Resource("Shooter") {
-    private val topMotor = LoggedNeo(getConfig(14, "Top"))
-    private val bottomMotor = LoggedNeo(getConfig(13, "Bottom"))
-
-    private val topPid = PIDController(0.0, 0.0, 0.0)
-    private val bottomPid = PIDController(0.0, 0.0, 0.0)
+    private val topMotor = LoggedNeo(getConfig(14, "Top", false))
+    private val bottomMotor = LoggedNeo(getConfig(13, "Bottom", true))
 
     private var state = State.IDLE
 
-    enum class State(val getTopVoltage: () -> Double, val getBottomVoltage: () -> Double) {
-        IDLE({ 0.0 }, { 0.0 }),
-        SHOOT({ 11.0 }, { -5.0 }),
-        AMP({ 2.5 }, { -5.0 });
+    enum class State(val topVoltage: Double, val bottomVoltage: Double) {
+        IDLE(0.0, 0.0),
+        SHOOT(11.0, 5.0),
+        AMP(0.7, 4.6);
     }
 
-    init {
-        CoroutineRobot.startPeriodic {
-            val topVoltage = topPid.calculate(topMotor.getAngle().rotations, state.getTopVoltage())
-            val bottomVoltage = bottomPid.calculate(bottomMotor.getAngle().rotations, state.getBottomVoltage())
-
-//            topMotor.setVoltage(topVoltage)
-//            bottomMotor.setVoltage(bottomVoltage)
-        }
-    }
-
-//    override val defaultAction: Action = {
-//        state = State.IDLE
-//    }
-
-    suspend fun spinUpToState(state: State) {
+    fun setState(state: State) {
         Shooter.state = state
-        topMotor.setVoltage(state.getTopVoltage())
-        bottomMotor.setVoltage(state.getBottomVoltage())
-//        while (!topPid.atSetpoint() && !bottomPid.atSetpoint()) delay(20)
+        topMotor.setVoltage(state.topVoltage)
+        bottomMotor.setVoltage(state.bottomVoltage)
     }
 
-    private fun getConfig(canID: Int, additionalQualifier: String) = LoggedNeo.Config(
+    private fun getConfig(canID: Int, additionalQualifier: String, inverted: Boolean) = LoggedNeo.Config(
         canID = canID,
         deviceName = "Shooter",
         gearRatio = 1.0,
@@ -51,7 +31,7 @@ object Shooter: Resource("Shooter") {
         motorType = Spark.MotorType.VORTEX,
         simJkgMetersSquared = 0.003,
         sparkConfig = Spark.Config(
-            inverted = false
+            inverted = inverted
         )
     )
 }
