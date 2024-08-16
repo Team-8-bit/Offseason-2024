df58ec1e039da35b85cf6a4f6534beabb8dddb55
nolan <nolan.s.locke@gmail.com>
2024-08-16 12:18:18 -0700

    add lots of autos

--- robot/src/main/kotlin/org/team9432/auto/FourNote.kt
+++ robot/src/main/kotlin/org/team9432/auto/FourNote.kt
@@ -18,22 +18,20 @@ import org.team9432.resources.swerve.Swerve
 import kotlin.time.Duration.Companion.seconds
 
 object FourNote {
-    suspend fun runFourNote() {
+    suspend fun runFourAndNothing() {
         val trajectories = ChoreoTrajectories.fourAndNothing
         val (firstPath, ampNote, speakerNote, stageNote) = trajectories
 
-        Swerve.seedFieldRelative(trajectories.first().getAutoFlippedInitialPose())
+        runGenericFourNote(firstPath, ampNote, speakerNote, stageNote)
 
-        Shooter.setState(Shooter.State.VISION_SHOOT)
-        parallel(
-            { Swerve.followChoreo(firstPath) },
-            { simDelay(1.seconds) }
-        )
-        shootNote()
+        Shooter.setState(Shooter.State.IDLE)
+    }
 
-        scoreNote(ampNote)
-        scoreNote(speakerNote)
-        scoreNote(stageNote)
+    suspend fun runReverseFourAndNothing() {
+        val trajectories = ChoreoTrajectories.reverseFourAndNothing
+        val (firstPath, ampNote, speakerNote, stageNote) = trajectories
+
+        runGenericFourNote(firstPath, ampNote, speakerNote, stageNote)
 
         Shooter.setState(Shooter.State.IDLE)
     }
@@ -42,7 +40,78 @@ object FourNote {
         val trajectories = ChoreoTrajectories.fourAndCenter
         val (firstPath, ampNote, speakerNote, stageNote, center) = trajectories
 
-        Swerve.seedFieldRelative(trajectories.first().getAutoFlippedInitialPose())
+        runGenericFourNote(firstPath, ampNote, speakerNote, stageNote)
+
+        Shooter.setState(Shooter.State.IDLE)
+        Swerve.followChoreo(center)
+    }
+
+    suspend fun runCenter54() {
+        val trajectories = ChoreoTrajectories.center54
+        val (note5, note4) = trajectories
+
+        runGenericDoubleCenter(note5, note4)
+
+        Shooter.setState(Shooter.State.IDLE)
+    }
+
+    suspend fun runCenter12() {
+        val trajectories = ChoreoTrajectories.center12
+        val (note1, note2) = trajectories
+
+        runGenericDoubleCenter(note1, note2)
+
+        Shooter.setState(Shooter.State.IDLE)
+    }
+
+    suspend fun runFourAndCenter1() {
+        val trajectories = ChoreoTrajectories.fourAndCenter1
+        val (firstPath, ampNote, speakerNote, stageNote, centerNote) = trajectories
+
+        genericFourAndCenter(firstPath, ampNote, speakerNote, stageNote, centerNote)
+    }
+
+    suspend fun runFourAndCenter2() {
+        val trajectories = ChoreoTrajectories.fourAndCenter2
+        val (firstPath, ampNote, speakerNote, stageNote, centerNote) = trajectories
+
+        genericFourAndCenter(firstPath, ampNote, speakerNote, stageNote, centerNote)
+    }
+
+    suspend fun runAmpAndCenter12() {
+        val trajectories = ChoreoTrajectories.ampAndCenter12
+        val (ampNote, note1, note2) = trajectories
+
+        Swerve.seedFieldRelative(ampNote.getAutoFlippedInitialPose())
+
+        Shooter.setState(Shooter.State.VISION_SHOOT)
+
+        simDelay(1.seconds) // Fake flywheel spinup
+        shootNote()
+
+        scoreNotes(ampNote, note1, note2)
+    }
+
+    private suspend fun genericFourAndCenter(
+        firstPath: ChoreoTrajectory,
+        firstNote: ChoreoTrajectory,
+        secondNote: ChoreoTrajectory,
+        thirdNote: ChoreoTrajectory,
+        centerNote: ChoreoTrajectory,
+    ) {
+        runGenericFourNote(firstPath, firstNote, secondNote, thirdNote)
+        scoreNote(centerNote)
+
+        Shooter.setState(Shooter.State.IDLE)
+    }
+
+    private suspend fun runGenericFourNote(
+        firstPath: ChoreoTrajectory,
+        firstNote: ChoreoTrajectory,
+        secondNote: ChoreoTrajectory,
+        thirdNote: ChoreoTrajectory,
+    ) {
+        Swerve.seedFieldRelative(firstPath.getAutoFlippedInitialPose())
 
         Shooter.setState(Shooter.State.VISION_SHOOT)
         parallel(
@@ -51,13 +120,24 @@ object FourNote {
         )
         shootNote()
 
-        scoreNote(ampNote)
-        scoreNote(speakerNote)
-        scoreNote(stageNote)
-
-        Shooter.setState(Shooter.State.IDLE)
-        Swerve.followChoreo(center)
+        scoreNotes(firstNote, secondNote, thirdNote)
     }
+
+    private suspend fun runGenericDoubleCenter(
+        firstNote: ChoreoTrajectory,
+        secondNote: ChoreoTrajectory,
+    ) {
+        Swerve.seedFieldRelative(firstNote.getAutoFlippedInitialPose())
+
+        Shooter.setState(Shooter.State.VISION_SHOOT)
+
+        simDelay(1.seconds) // Fake flywheel spinup
+        shootNote()
+
+        scoreNotes(firstNote, secondNote)
+    }
+
+    private suspend fun scoreNotes(vararg notes: ChoreoTrajectory) = notes.forEach { scoreNote(it) }
 
     private suspend fun scoreNote(ampNote: ChoreoTrajectory) = coroutineScope {
         val intakingJob = launch { Actions.intake() }
