5f214b19a677932b5ded6437f3d7ae89247455e7
Nolan L <nolan.s.locke@gmail.com>
2024-08-25 01:10:38 -0700

    add configurable auto chooser (needs cleanup but if our house burns down or something without this saved I'm not doing it again)

--- robot/src/main/kotlin/org/team9432/auto/Auto.kt
+++ robot/src/main/kotlin/org/team9432/auto/Auto.kt
@@ -1,28 +1,13 @@
 package org.team9432.auto
 
-import com.choreo.lib.Choreo
-import com.choreo.lib.ChoreoTrajectory
-
-
-object ChoreoTrajectories {
-    val fourAndNothing: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("4AndNothing")
-    val reverseFourAndNothing: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("Reverse4AndNothing")
-    val fourAndCenter: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("4AndCenter")
-    val fourAndCenter1: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("4AndCenter1")
-    val fourAndCenter2: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("4AndCenter2")
-    val center54: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("Center54")
-    val center12: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("Center12")
-    val ampAndCenter12: ArrayList<ChoreoTrajectory> = Choreo.getTrajectoryGroup("AmpAndCenter12")
-}
-
 enum class Auto(val prettyName: String, val auto: suspend () -> Unit) {
-    FOUR_AND_NOTHING("Four and Nothing", FourNote::runFourAndNothing),
-    REVERSE_FOUR_AND_NOTHING("Reversed Four and Nothing", FourNote::runReverseFourAndNothing),
-    FOUR_AND_CENTER("Four and Center", FourNote::runFourNoteCenter),
-    FOUR_AND_CENTER_1("Four and Center 1", FourNote::runFourAndCenter1),
-    FOUR_AND_CENTER_2("Four and Center 2", FourNote::runFourAndCenter2),
-    CENTER_54("Center 54", FourNote::runCenter54),
-    CENTER_12("Center 12", FourNote::runCenter12),
-    AMP_AND_CENTER_12("Amp And Center 12", FourNote::runAmpAndCenter12),
-    NOTHING("Do Nothing", {})
+//    FOUR_AND_NOTHING("Four and Nothing", FourNote::runFourAndNothing),
+//    REVERSE_FOUR_AND_NOTHING("Reversed Four and Nothing", FourNote::runReverseFourAndNothing),
+//    FOUR_AND_CENTER("Four and Center", FourNote::runFourNoteCenter),
+//    FOUR_AND_CENTER_1("Four and Center 1", FourNote::runFourAndCenter1),
+//    FOUR_AND_CENTER_2("Four and Center 2", FourNote::runFourAndCenter2),
+//    CENTER_54("Center 54", FourNote::runCenter54),
+//    CENTER_12("Center 12", FourNote::runCenter12),
+//    AMP_AND_CENTER_12("Amp And Center 12", FourNote::runAmpAndCenter12),
+//    NOTHING("Do Nothing", {})
 }
--- robot/src/main/kotlin/org/team9432/auto/types/AutoType.kt
+++ robot/src/main/kotlin/org/team9432/auto/types/AutoType.kt
@@ -1,5 +1,6 @@
 package org.team9432.auto.types
 
+import org.team9432.auto.AutoSelectorOptions
 import org.team9432.auto.paths.AutoFieldConstants.CenterNote
 import org.team9432.auto.paths.AutoFieldConstants.CenterNote.*
 
@@ -31,6 +32,46 @@ object AutoType {
                 for (ampFirst in setOf(false, true))
                     for (centerNote in validCenterNotes)
                         add(FourNote(ampFirst, centerNote))
+            }
+
+            enum class EndAction {
+                NOTHING, DRIVE_TO_CENTER, SCORE_CENTER
+            }
+
+            var ampFirst = true
+            var centerNote: CenterNote? = null
+            var endAction: EndAction = EndAction.NOTHING
+
+            fun AutoSelectorOptions.applyFourNoteSelectorOptions() {
+                addQuestion("Start Note") {
+                    addOption("Amp") { ampFirst = true }
+                    addOption("Stage") { ampFirst = false }
+                }
+
+                addQuestion("End Action") {
+                    addOption("Nothing") { endAction = EndAction.NOTHING }
+
+                    addOption("Drive To Center") {
+                        endAction = EndAction.DRIVE_TO_CENTER
+
+                        addQuestion("Center End Position") {
+                            //examples
+                            addOption("Source") {}
+                            addOption("Center") {}
+                            addOption("Amp") {}
+                        }
+                    }
+
+                    addOption("Score Centerline") {
+                        endAction = EndAction.SCORE_CENTER
+
+                        addQuestion("Which Note") {
+                            validCenterNotes.filterNotNull().forEach {
+                                addOption(it.readableName) { centerNote = it }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
