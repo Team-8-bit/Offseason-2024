0218efc3dcb12c7c0e7e16f24dacdb0bc7b00b41
Nolan L <nolan.s.locke@gmail.com>
2024-08-27 00:30:26 -0700

    change four note amp pickup to match the stage

--- robot/src/main/kotlin/org/team9432/auto/paths/FourNotePaths.kt
+++ robot/src/main/kotlin/org/team9432/auto/paths/FourNotePaths.kt
@@ -5,6 +5,7 @@ import org.team9432.auto.paths.AutoFieldConstants.CenterNote.*
 import org.team9432.auto.paths.AutoFieldConstants.CloseNote.*
 import org.team9432.auto.types.AutoType
 import org.team9432.choreogenerator.ChoreoTrajectory
+import org.team9432.choreogenerator.MaxAngVelocity
 import org.team9432.choreogenerator.Position
 import org.team9432.choreogenerator.StraightLine
 import org.team9432.lib.unit.degrees
@@ -16,15 +17,15 @@ object FourNotePaths {
 
     private val FOUR_NOTE_START = Position(1.34.meters, AutoFieldConstants.speakerYCoordinate, 180.0.degrees)
     private val FOUR_NOTE_SHOT = Position(1.7.meters, AutoFieldConstants.speakerYCoordinate, 180.0.degrees)
-    private val FOUR_NOTE_AMP_ALIGN = AutoFieldConstants.ampNote.copy().moveTowards(FOUR_NOTE_SHOT, 0.75.meters).pointAwayFrom(AutoFieldConstants.ampNote)
-    private val FOUR_NOTE_AMP_PICKUP = AutoFieldConstants.ampNote.copy().moveTowards(FOUR_NOTE_SHOT, 0.15.meters).pointAwayFrom(AutoFieldConstants.ampNote)
-    private val FOUR_NOTE_SPEAKER_PICKUP =
-        Position(2.88.meters, AutoFieldConstants.speakerYCoordinate, 180.0.degrees)/*This next bit is because of a choreo bug, not trying to align better*/.moveX(-3.inches)
+    private val FOUR_NOTE_AMP_ALIGN = AutoFieldConstants.ampNote.copy().moveX(-0.6.meters).pointAwayFrom(AutoFieldConstants.ampNote)
+    private val FOUR_NOTE_AMP_PICKUP = AutoFieldConstants.ampNote.copy().moveX(-0.2.meters).pointAwayFrom(AutoFieldConstants.ampNote)
+    private val FOUR_NOTE_AMP_EXIT = AutoFieldConstants.ampNote.copy().move(x = -0.5.meters, y = -0.5.meters, heading = 180.degrees)
+    private val FOUR_NOTE_SPEAKER_PICKUP = Position(2.88.meters, AutoFieldConstants.speakerYCoordinate, 180.0.degrees)/*This part is because of a choreo bug, not to align better*/.moveX(-3.inches)
     private val FOUR_NOTE_STAGE_ALIGN = AutoFieldConstants.stageNote.copy().moveX(-0.6.meters).pointAwayFrom(AutoFieldConstants.stageNote)
     private val FOUR_NOTE_STAGE_PICKUP = AutoFieldConstants.stageNote.copy().moveX(-0.2.meters).pointAwayFrom(AutoFieldConstants.stageNote)
-    private val FOUR_NOTE_STAGE_EXIT = AutoFieldConstants.stageNote.copy().move(x = -0.5.meters, y = 0.5.meters, heading = 180.degrees)//.pointAwayFrom(AutoFieldConstants.stageNote)
+    private val FOUR_NOTE_STAGE_EXIT = AutoFieldConstants.stageNote.copy().move(x = -0.5.meters, y = 0.5.meters, heading = 180.degrees)
 
-    private val FOUR_NOTE_ABOVE_STAGE_DRIVE = Position(5.5.meters, 6.5.meters, 180.0.degrees)
+    private val FOUR_NOTE_ABOVE_STAGE_DRIVE = Position(5.5.meters, 6.3.meters, 180.0.degrees)
     private val FOUR_NOTE_THROUGH_STAGE_DRIVE_CLOSE = Position(4.4.meters, 5.meters, 180.0.degrees)
     private val FOUR_NOTE_THROUGH_STAGE_DRIVE_MIDDLE = Position(5.meters, 4.3.meters, 180.0.degrees)
     private val FOUR_NOTE_THROUGH_STAGE_DRIVE_FAR = Position(6.meters, 4.meters, 180.0.degrees)
@@ -97,8 +98,10 @@ object FourNotePaths {
 
         when (note) {
             AMP -> {
-                addPoseWaypoint(FOUR_NOTE_AMP_ALIGN)
-                addPoseWaypoint(FOUR_NOTE_AMP_PICKUP)
+                val align = addPoseWaypoint(FOUR_NOTE_AMP_ALIGN)
+                val pickup = addPoseWaypoint(FOUR_NOTE_AMP_PICKUP)
+                addConstraint(StraightLine(align, pickup))
+                addTranslationWaypoint(FOUR_NOTE_AMP_EXIT)
                 addPoseWaypoint(FOUR_NOTE_SHOT, stopPoint = true)
             }
 
@@ -108,9 +111,9 @@ object FourNotePaths {
             }
 
             STAGE -> {
-                val straightLineStart = addPoseWaypoint(FOUR_NOTE_STAGE_ALIGN)
-                val straightLineEnd = addPoseWaypoint(FOUR_NOTE_STAGE_PICKUP)
-                addConstraint(StraightLine(straightLineStart, straightLineEnd))
+                val align = addPoseWaypoint(FOUR_NOTE_STAGE_ALIGN)
+                val pickup = addPoseWaypoint(FOUR_NOTE_STAGE_PICKUP)
+                addConstraint(StraightLine(align, pickup))
                 addTranslationWaypoint(FOUR_NOTE_STAGE_EXIT)
                 addPoseWaypoint(FOUR_NOTE_SHOT, stopPoint = true)
             }
